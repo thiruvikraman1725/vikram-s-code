4d9f7ffe3259673d24cdcda0833086c0c6a74f7c
thiruvikraman1725 <thiruvikramansuguna@gmail.com>
2025-10-05 17:05:40 +0530

    Add files via upload

---
+++ JAVA PATTERN.java
@@ -0,0 +1,17 @@
+public class ooo{
+    public static void main(String[] args){
+        int n=4;
+        int i,j,k;
+        for(i=1;i<=n;i++){
+             for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }        
+                for(k=1;k<=2*(i)-1;k++){
+                    System.out.print("*");
+                }
+                  System.out.println();
+            }
+            }
+    }
+    
+
